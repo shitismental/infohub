0426e0cd0133b760c7ab21803f196fce0f86b022
CuteBuffy <cutebuffydiscord@gmail.com>
2025-10-08 22:16:11 +0300

    fix

--- utils/media.js
+++ utils/media.js
@@ -2,17 +2,6 @@ import { MEDIA_URL } from "@env";
 
 export const getMediaUrl = (path) => {
   if (!path) return "";
-
-  const backendPrefix = "http://152.53.18.199/media";
-
-  // If backend returned a full absolute URL
-  if (path.startsWith(backendPrefix)) {
-    return path.replace(backendPrefix, MEDIA_URL); 
-  }
-
-  // Already HTTPS or external
-  if (path.startsWith("https://")) return path;
-
-  // Otherwise assume relative path
-  return `${MEDIA_URL}${path}`;
+  if (process.env.NODE_ENV === "development") return path;
+  return path.replace("http://152.53.18.199/media", MEDIA_URL);
 };
